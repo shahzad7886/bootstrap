3801d440d410bbc317dedee835790032aaf9df9c
Asim Shahzad <113654383+shahzad7886@users.noreply.github.com>
2023-11-30 19:26:30 +0500

    push

--- components/Layout.js
+++ components/Layout.js
@@ -8,13 +8,7 @@ export default function Layout({children}) {
   const { data: session } = useSession();
   if (!session) {
     return (
-<<<<<<< HEAD
       <div className="bg-blue-900 w-screen  h-screen flex items-center">
-=======
-     
-
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
->>>>>>> parent of 22b9b97 (push)
         <div className="text-center w-full">
           <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
         </div>
